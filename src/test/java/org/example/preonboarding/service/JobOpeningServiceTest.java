bd403f4f8aa3be89b1c7306f4aa3fe19f146afe8
Hajin <qwertygoov@naver.com>
2024-08-10 22:54:11 +0900

    feat: 채용 공고 수정 테스트코드 작성

--- src/test/java/org/example/preonboarding/service/JobOpeningServiceTest.java
+++ src/test/java/org/example/preonboarding/service/JobOpeningServiceTest.java
@@ -3,6 +3,7 @@ package org.example.preonboarding.service;
 import org.example.preonboarding.domain.Company;
 import org.example.preonboarding.domain.JobOpening;
 import org.example.preonboarding.dto.CreateJobOpeningRequest;
+import org.example.preonboarding.dto.UpdateJobOpeningRequest;
 import org.example.preonboarding.exception.CustomException;
 import org.example.preonboarding.exception.ErrorCode;
 import org.example.preonboarding.repository.CompanyRepository;
@@ -86,4 +87,94 @@ class JobOpeningServiceTest {
         assertEquals(ErrorCode.COMPANY_NOT_FOUND, exception.getErrorCode());
     }
 
+    @Test
+    @DisplayName("채용 공고를 수정합니다.")
+    void update_job_opening() {
+        // given
+        Company company = new Company("원티드랩", "한국", "서울 송파구");
+        companyRepository.save(company);
+
+        JobOpening jobOpening = JobOpening.builder()
+                .companyId(company.getId())
+                .jobPosition("파이썬 개발자 [채용솔루션팀]")
+                .jobDescription("서비스 기능 제공을 위한 REST API 개발")
+                .techStack("FastApi, Flask, Django")
+                .signingBonus(1000000)
+                .build();
+        jobOpeningRepository.save(jobOpening);
+
+        UpdateJobOpeningRequest updateJobOpeningRequest = new UpdateJobOpeningRequest();
+        updateJobOpeningRequest.setJobPosition("파이썬 개발자 - 채용솔루션팀");
+        updateJobOpeningRequest.setJobDescription("서비스 기능 제공을 위한 REST API 개발");
+        updateJobOpeningRequest.setSigningBonus(500000);
+        updateJobOpeningRequest.setTechStack("FastApi, Flask, Django 등");
+
+        // when
+        jobOpeningService.updateJobOpening(jobOpening.getId(), company.getId(), updateJobOpeningRequest);
+
+        // then
+        JobOpening updatedJobOpening = jobOpeningRepository.findById(jobOpening.getId()).orElse(null);
+        assertNotNull(updatedJobOpening);
+        assertEquals(company.getId(), updatedJobOpening.getCompanyId());
+        assertEquals(updateJobOpeningRequest.getJobPosition(), updatedJobOpening.getJobPosition());
+        assertEquals(updateJobOpeningRequest.getJobDescription(), updatedJobOpening.getJobDescription());
+        assertEquals(updateJobOpeningRequest.getSigningBonus(), updatedJobOpening.getSigningBonus());
+        assertEquals(updateJobOpeningRequest.getTechStack(), updatedJobOpening.getTechStack());
+    }
+
+    @Test
+    @DisplayName("존재하지 않은 채용 공고를 수정하여 실패합니다.")
+    void update_nonexistent_job_opening() {
+        // given
+        Company company = new Company("원티드랩", "한국", "서울 송파구");
+        companyRepository.save(company);
+
+        UpdateJobOpeningRequest updateJobOpeningRequest = new UpdateJobOpeningRequest();
+        updateJobOpeningRequest.setJobPosition("파이썬 개발자 - 채용솔루션팀");
+        updateJobOpeningRequest.setJobDescription("서비스 기능 제공을 위한 REST API 개발");
+        updateJobOpeningRequest.setSigningBonus(500000);
+        updateJobOpeningRequest.setTechStack("FastApi, Flask, Django 등");
+
+        // when
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            jobOpeningService.updateJobOpening(404L, company.getId(), updateJobOpeningRequest);
+        });
+
+        // then
+        assertEquals(ErrorCode.JOB_OPENING_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("접근 권한이 없는 회사가 채용 공고를 수정하여 실패합니다.")
+    void update_job_opening_by_invalid_company() {
+        // given
+        Company company = new Company("원티드랩", "한국", "서울 송파구");
+        companyRepository.save(company);
+
+        Company invalidAccessCompany = new Company("인프랩", "한국", "경기 성남시");
+        companyRepository.save(company);
+
+        JobOpening jobOpening = JobOpening.builder()
+                .companyId(company.getId())
+                .jobPosition("파이썬 개발자 [채용솔루션팀]")
+                .jobDescription("서비스 기능 제공을 위한 REST API 개발")
+                .techStack("FastApi, Flask, Django")
+                .signingBonus(1000000)
+                .build();
+        jobOpeningRepository.save(jobOpening);
+
+        UpdateJobOpeningRequest updateJobOpeningRequest = new UpdateJobOpeningRequest();
+        updateJobOpeningRequest.setJobPosition("파이썬 개발자 - 채용솔루션팀");
+        updateJobOpeningRequest.setJobDescription("서비스 기능 제공을 위한 REST API 개발");
+        updateJobOpeningRequest.setSigningBonus(500000);
+        updateJobOpeningRequest.setTechStack("FastApi, Flask, Django 등");
+
+        // when
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            jobOpeningService.updateJobOpening(jobOpening.getId(), invalidAccessCompany.getId(), updateJobOpeningRequest);
+        });
+
+        // then
+        assertEquals(ErrorCode.INVALID_COMPANY_ACCESS, exception.getErrorCode());
+    }
 }
